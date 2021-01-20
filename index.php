868759074164d6f32e5d40845955bf39cde69ce4
Jonas zanarotti <76817664+Jonaszan-DevMaster@users.noreply.github.com>
2021-01-20 14:30:33 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Descrição do site">
+        <meta name="Keywords" content="Palavras Chave">
+        <link rel="stylesheet" href="fontawesome/css/all.css">
+        <link rel="stylesheet" type="text/css" href="style/main.css">
+        <title>Projeto 01</title>
+</head>
+
+<body>
+
+        <!--  Começa Cabeçalho-->
+        <header>
+                <div class="center">
+                        <div class="logo">2.B Systems</div><!--  logo -->
+                        <nav class="desktop right">
+                                <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Sobre</a></li>
+                                        <li><a href="#">Serviços</a></li>
+                                        <li><a href="#">Contato</a></li>
+                                </ul>
+                        </nav>
+                        <nav class="mobile right">
+                                <div class="btn-mobile-menu">
+                                        <i class="fas fa-bars"></i>
+                                </div>
+                                <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Sobre</a></li>
+                                        <li><a href="#">Serviços</a></li>
+                                        <li><a href="#">Contato</a></li>
+                                </ul>
+                        </nav>
+                        <div class="clear"></div><!-- Clear Both-->
+                </div> <!--  center -->
+        </header>
+
+        <!--  Termina Cabeçalho-->
+
+        <!--  Começa Banner-->
+        <section class="banner-principal">
+                <div class="overlay"></div><!--  overlay -->
+                <div class="center">
+                        <form>
+                                <h2>Qual seu melhor <e-mail>email</e-mail>
+                                </h2>
+                                <input type="email" name="email" required />
+                                <input type="submit" name="acao" value="CADASTRAR" />
+                        </form>
+                </div> <!--  center -->
+        </section><!--  banner-principal -->
+
+        <!--  Termina Banner-->
+
+        <section class="descricao-autor">
+        <div class="center">
+                <div class="w50 left">
+                        <h2>Jonaszan</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, officiis dolores
+                                quod molestiae cupiditate molestias dolor vero ipsa saepe laboriosam nihil ad
+                                suscipit cumque amet eaque blanditiis adipisci cum harum.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, officiis dolores
+                                quod molestiae cupiditate molestias dolor vero ipsa saepe laboriosam nihil ad
+                                suscipit cumque amet eaque blanditiis adipisci cum harum.</p>
+                </div> <!--  w50 -->
+                <div class="w50 left">
+                        <img src="img/perfil_author2.jpg" class="img-author" />
+                </div> <!--  w50 -->
+                </div> <!--  center -->
+                <div class="clear"></div>
+        </section> <!--  descricao-autor-->
+
+        <!-- Começa  especialidades -->
+        <section class="especialidades">
+
+                <h2 class="title">Especialidades</h2>
+                <div class="center">
+                        <div class="w33 left box-especialidades">
+                                <h3><i class="fab fa-css3-alt"></i></h3>
+                                <h4>CSS3</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi optio
+                                        voluptatem odit consequatur unde hic repudiandae facilis nihil nisi iste quas
+                                        voluptas molestiae aspernatur nam, modi explicabo nobis! Iste, cumque!</p>
+                        </div> <!--  box-especialidades -->
+
+                        <div class="w33 left box-especialidades">
+                                <h3><i class="fab fa-html5"></i></h3>
+                                <h4>HTML</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi optio
+                                        voluptatem odit consequatur unde hic repudiandae facilis nihil nisi iste quas
+                                        voluptas molestiae aspernatur nam, modi explicabo nobis! Iste, cumque!</p>
+                        </div> <!--  box-especialidades -->
+
+                        <div class="w33 left box-especialidades">
+                                <h3><i class="fab fa-js"></i></h3>
+                                <h4>JAVASCRIPT</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi optio
+                                        voluptatem odit consequatur unde hic repudiandae facilis nihil nisi iste quas
+                                        voluptas molestiae aspernatur nam, modi explicabo nobis! Iste, cumque!</p>
+                        </div> <!--  box-especialidades -->
+                        <div class="clear"></div><!-- Clear Both-->
+                </div><!--  center -->
+        </section><!--  especialidades -->
+
+        <section class="extras">
+
+                <div class="center">
+
+                        <div class="w50 left">
+                                <h2 class="title">Depoimentos</h2>
+                                <div class="depoimento-single">
+                                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                cupiditate rem necessitatibus tenetur! Doloribus natus nemo dignissimos?</p><!-- depoimento-descricao  -->
+                                        <p class="nome-autor">Author Name </p><!-- nome-autor  -->
+                                </div> <!--  depoimento-single -->
+                                <div class="depoimento-single">
+                                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                cupiditate rem necessitatibus tenetur! Doloribus natus nemo dignissimos?</p><!-- depoimento-descricao  -->
+                                        <p class="nome-autor">Author Name </p><!-- nome-autor  -->
+                                </div> <!--  depoimento-single -->
+                                <div class="depoimento-single">
+                                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                cupiditate rem necessitatibus tenetur! Doloribus natus nemo dignissimos?</p><!-- depoimento-descricao  -->
+                                        <p class="nome-autor">Author Name</p><!-- nome-autor  -->
+                                </div> <!--  depoimento-single -->
+                        </div> <!--  w50 -->
+                        <div class="w50 left">
+                                <h2 class="title-serviços">Serviços</h2>
+                                <div class="servicos">
+                                        <ul>
+                                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                        harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                        cupiditate rem necessitatibus tenetur!</li>
+                                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                        harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                        cupiditate rem necessitatibus tenetur!</li>
+                                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis perferendis
+                                                        harum aliquid, quam eum ut amet eligendi dolorum ducimus dicta debitis iusto
+                                                        cupiditate rem necessitatibus tenetur!</li>
+                                        </ul>
+                                </div><!--  servicos -->
+                        </div><!--  w50 -->
+                        <div class="clear"></div>
+                </div> <!--  center -->
+        </section> <!--  extras -->
+        <footer>
+                <div class="center">
+                        <p><copyright>&copy Todos os direitos reservados</copyright></p>
+                </div><!--  center -->
+        </footer>
+
+
+</body>
+
+</html>
